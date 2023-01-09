84f5aef3dbd99da42f7d9e9b0d4b3580ab08b627
Dhairya <dhairyagulati370@gmail.com>
2023-01-09 17:55:46 +0530

    Add: Form & CSS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Form from "./form/Form";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
 }
 
 export default App;
--- src/form/Form.jsx
+++ src/form/Form.jsx
@@ -2,7 +2,26 @@ import React from "react";
 import "./Form.css";
 
 function Form() {
-  return <div>Form</div>;
+  return (
+    <div className="form-container">
+      <div className="form">
+        <h2>Enter your Details</h2>
+        <input id="firstName" placeholder="firstname"></input>
+        <input id="lastName" placeholder="lastname"></input>
+        <div className="captcha">
+          <div className="cpatcha-image">
+            <input id="captcha-background" value="hi" disabled="true"></input>
+            <button>re</button>
+          </div>
+          <div className="captcha">
+            <input id="type-captcha"></input>
+            <button>check</button>
+          </div>
+          <p></p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Form;
