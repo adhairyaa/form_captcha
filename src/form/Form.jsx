868cda390c6c9ef12e1caa598b1454567c206ecf
Dhairya <dhairyagulati370@gmail.com>
2023-01-09 18:32:47 +0530

    Add : Captcha logic

--- src/form/Form.jsx
+++ src/form/Form.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Form.css";
 
 function Form() {
+  const [captcha, setCaptcha] = useState("");
+  const [userInput, setUserInput] = useState();
+
+  const [responseMessage, setResponseMessage] = useState();
+
+  const generateCaptcha = () => {
+    setCaptcha((Math.random() + 1).toString(36).substring(7));
+  };
+
+  const validateCaptcha = () => {};
+
   return (
     <div className="form-container">
       <div className="form">
@@ -9,15 +20,19 @@ function Form() {
         <input id="firstName" placeholder="firstname"></input>
         <input id="lastName" placeholder="lastname"></input>
         <div className="captcha">
-          <div className="cpatcha-image">
-            <input id="captcha-background" value="hi" disabled="true"></input>
-            <button>re</button>
+          <div className="captcha-image">
+            <input
+              id="captcha-background"
+              value={captcha}
+              disabled="true"
+            ></input>
+            <button onClick={() => generateCaptcha()}>⟲</button>
           </div>
-          <div className="captcha">
-            <input id="type-captcha"></input>
-            <button>check</button>
+          <div className="captcha-validate">
+            <input id="type-captcha" onChange={(e) => setUserInput(e)}></input>
+            <button onClick={() => validateCaptcha()}>check</button>
           </div>
-          <p></p>
+          <p>{responseMessage}</p>
         </div>
       </div>
     </div>
